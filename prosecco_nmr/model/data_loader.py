dcc1f651c317a1ab0ad188d9b1e2d93ed4007e0c
maximosanz <maximo.sanzh@gmail.com>
2020-03-10 17:50:09 +1100

    NLP_array_padding

--- prosecco_nmr/model/data_loader.py
+++ prosecco_nmr/model/data_loader.py
@@ -274,12 +274,12 @@ def make_NN_arrays(EntryDB,
 			cs_margin = np.zeros((NLP_margins,N_Atoms))
 			cs_margin[:] = np.nan
 			NLP_CS = np.concatenate([cs_margin,cs_Arr,cs_margin],axis=0)
-			total_length = NLP_segment_length + 2 * NLP_margins
 			# Right-padding of sequences shorter than NLP_segment_length:
-			if NLP_Arr.shape[0] < total_length:
-				N_pad = total_length-NLP_Arr.shape[0]
+			if NLP_Arr.shape[0] < NLP_segment_length:
+				N_pad = NLP_segment_length-NLP_Arr.shape[0]
 				rightpad = np.tile(margin_col,(N_pad,1))
 				cs_rightpad = np.zeros((N_pad,N_Atoms))
+				cs_rightpad[:] = np.nan
 				NLP_Arr = np.concatenate([NLP_Arr,rightpad],axis=0)
 				NLP_CS = np.concatenate([NLP_CS,cs_rightpad],axis=0)
 			for pos in range(0,NLP_Arr.shape[0]-NLP_segment_length,NLP_segment_stride):
